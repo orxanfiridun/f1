18ea128c4c469f29b9fd22fdf05acacd410e0711
rahimfirudunlu <rahim@workstreams.ai>
2022-10-14 18:21:02 +0400

    refactor

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,8 +5,6 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as Yup from "yup";
 import Input from "./Input";
-import facebook from "../components/assets/facebook.png";
-import google from "../components/assets/google.png";
 import leftSide from "../components/assets/leftSide.jpg";
 
 function Main() {
@@ -38,69 +36,6 @@ function Main() {
     return false;
   }
 
-  // const onSubmit = (data) => console.log(data);
-  // const [fullname, setFullname] = useState("");
-  // const [surname, setSurname] = useState("");
-  // const [email, setEmail] = useState("");
-  // const [password, setPassword] = useState("");
-  // const [repassword, setRePassword] = useState("");
-  // const [gender, setGender] = useState("");
-
-  // const [errorFullname, setErrorFullname] = useState("");
-  // const [errorSurname, setErrorSurname] = useState("");
-  // const [errorEmail, setErrorEmail] = useState("");
-  // const [errorPassword, setErrorPassword] = useState("");
-  // const [errorRePassword, setErrorRePassword] = useState("");
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   if (gender === "male") {
-  //     console.log(gender);
-  //   }
-  //   if (gender === "female") {
-  //     console.log(gender);
-  //   }
-  //   if (fullname !== "") {
-  //     if (fullname.length < 8) {
-  //       setErrorFullname("fullname has min 8 kharakter");
-  //     } else {
-  //       setErrorFullname("");
-  //     }
-
-  //     console.log(fullname);
-  //   } else {
-  //     setErrorFullname("fullname is invalid");
-  //   }
-  //   if (surname !== "") {
-  //     setErrorSurname("");
-  //     console.log(surname);
-  //   } else {
-  //     setErrorSurname("surname is invalid");
-  //   }
-  //   if (email !== "") {
-  //     setErrorEmail("");
-  //     console.log(email);
-  //   } else {
-  //     setErrorEmail("email is invalid");
-  //   }
-  //   if (password !== "") {
-  //     setErrorPassword("");
-  //     console.log(password);
-  //   } else {
-  //     setErrorPassword("password is invalid");
-  //   }
-  //   if (repassword !== "") {
-  //     if (password !== repassword) {
-  //       setErrorRePassword("check the correctness of the passwords ");
-  //     } else {
-  //       setErrorRePassword("");
-  //     }
-
-  //     console.log(repassword);
-  //   } else {
-  //     setErrorRePassword("repassword is invalid");
-  //   }
-  // };
-
   return (
     <div className="main">
       <div className="LeftSide">
@@ -109,76 +44,70 @@ function Main() {
       <div className="RightSide">
         <div className="language">English (UK)</div>
         <h1>Create Account</h1>
-
         <div className="signup">
-          <div className="signup-google">
-            <div className="signimg">
-              <img alt={google} />
+          <div className="signup__button">
+            <div className="signup__image">
+              <img src="/google.png" alt="google" />
             </div>
-            <div className="signdiv">Sign up with Google</div>
+            <div className="signup__title">Sign up with Google</div>
           </div>
-          <div className="signup-facebook">
-            <div className="fbimg">
-              <img alt={facebook} />
+          <div className="signup__button">
+            <div className="signup__image">
+              <img src="/facebook.png" alt="fb" />
             </div>
-            <div className="fbdiv">Sign up with Facebook</div>
+            <div className="signup__title">Sign up with Facebook</div>
           </div>
-          <div className="or">- OR -</div>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <Input
-              error={errors?.fullName}
-              errorMessage={errors?.fullName?.message}
-              placeholder="Full name"
-              {...register("fullName")}
-            />
-            <Input
-              error={errors?.surName}
-              errorMessage={errors?.surName?.message}
-              placeholder="Surname"
-              {...register("surName")}
-            />
-            <Input
-              error={errors?.email}
-              errorMessage={errors?.email?.message}
-              placeholder="Email"
-              type="email"
-              {...register("email")}
-            />
-            <Input
-              error={errors?.password}
-              errorMessage={errors?.password?.message}
-              placeholder="Password"
-              type="password"
-              {...register("password")}
-            />
-            <Input
-              error={errors?.repassword}
-              errorMessage={errors?.repassword?.message}
-              placeholder="Re Password"
-              type="password"
-              {...register("repassword")}
-            />
+        </div>
+        <div className="divider">- OR -</div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Input
+            error={errors?.fullName}
+            errorMessage={errors?.fullName?.message}
+            placeholder="Full name"
+            {...register("fullName")}
+          />
+          <Input
+            error={errors?.surName}
+            errorMessage={errors?.surName?.message}
+            placeholder="Surname"
+            {...register("surName")}
+          />
+          <Input
+            error={errors?.email}
+            errorMessage={errors?.email?.message}
+            placeholder="Email"
+            type="email"
+            {...register("email")}
+          />
+          <Input
+            error={errors?.password}
+            errorMessage={errors?.password?.message}
+            placeholder="Password"
+            type="password"
+            {...register("password")}
+          />
+          <Input
+            error={errors?.repassword}
+            errorMessage={errors?.repassword?.message}
+            placeholder="Re Password"
+            type="password"
+            {...register("repassword")}
+          />
 
-            <select className="select" {...register("gender")}>
-              <option
-                className="gender"
-                defaultValue="gender"
-                selected
-                disabled
-              >
-                Gender
-              </option>
-              <option value="male">Male</option>
-              <option value="female">Female </option>
-            </select>
-            <div className="create-account">
-              <button type="submit">Create Account</button>
-            </div>
-          </form>
-          <div className="already">
-            <div className="alrhv">Already have a account?</div>
-            <div className="login">Log in</div>
+          <select className="select" {...register("gender")}>
+            <option className="gender" defaultValue="gender" selected disabled>
+              Gender
+            </option>
+            <option value="male">Male</option>
+            <option value="female">Female </option>
+          </select>
+          <div className="create-account">
+            <button type="submit">Create Account</button>
           </div>
+        </form>
+        <div className="already">
+          <div className="alrhv">Already have a account?</div>
+          <div className="login">Log in</div>
         </div>
       </div>
     </div>
